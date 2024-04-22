14ec96c89e7587815c9097678c5fdb890da51502
Majdi-si <majdi.si.2003@gmail.com>
2024-04-22 09:36:50 +0200

    Supprimer les fichiers de configuration et les fichiers de démarrage du serveur web et de l'application Flask

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-from config import DEBUG, WEB_SERVER
-from views import app
+from sujet_groupeE.config import DEBUG, WEB_SERVER
+from sujet_groupeE.views import app
 if __name__ == '__main__':
     app.run(
     host = WEB_SERVER['host'],
--- sujet_groupeE/views.py
+++ sujet_groupeE/views.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.template_folder = "template"
 app.static_folder = "static"
-app.config.from_object('myApp.config')
+app.config.from_object('sujet_groupeE.config')
 @app.route("/")
 def index():
     return render_template("index.html")
