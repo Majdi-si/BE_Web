1ad3d5bdb580005a8f9d4ec385320418901f9697
Majdi-si <majdi.si.2003@gmail.com>
2024-04-29 19:06:04 +0200

    Supprimer les fichiers de configuration et les fichiers de démarrage du serveur web et de l'application Flask.

--- sujet_groupeE/views.py
+++ sujet_groupeE/views.py
@@ -6,3 +6,34 @@ app.config.from_object('sujet_groupeE.config')
 @app.route("/")
 def index():
     return render_template("index.html")
+
+#page home
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
+
+@app.route("/404")
+def error():
+    return render_template("404.html")
+
+@app.route("/chackout")
+def chackout():
+    return render_template("chackout.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/shop-detail")
+def shop_detail():
+    return render_template("shop-detail.html")
+
+@app.route("/shop")
+def shop():
+    return render_template("shop.html")
+
+@app.route("/testimonial")
+def testimonial():
+    return render_template("testimonial.html")
+    
+
